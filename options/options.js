09c349dd843646582062d3e7a07fd25797a7013c
Crowdbotics <team@crowdbotics.com>
2021-09-15 00:00:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "delicate_wind_30550Identifier",
+  url: "https://delicate_wind_30550Identifier.botics.co",
+  api: "https://delicate_wind_30550Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
